07c516ea576e9a294f4e1574b701e22f8dbc60ba
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-04 21:10:47 -0300

    encapsulamento

--- poo/src/main/java/com/example/relacionamento/Main.java
+++ poo/src/main/java/com/example/relacionamento/Main.java
@@ -2,7 +2,9 @@ package com.example.relacionamento;
 
 public class Main {
     public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa("joao", 30, 001, 2000.00, Sexo.MASCULINO, UnidadeFederativa.BAHIA);
         
+        System.out.println(pessoa1.toString());
     }
 
 }
--- poo/src/main/java/com/example/relacionamento/Pessoa.java
+++ poo/src/main/java/com/example/relacionamento/Pessoa.java
@@ -2,6 +2,61 @@ package com.example.relacionamento;
 
 public class Pessoa {
     private String nome;
+    public Pessoa(String nome, int idade, int id, double salario, Sexo sexo, UnidadeFederativa estado) {
+        this.nome = nome;
+        this.idade = idade;
+        this.id = id;
+        this.salario = salario;
+        this.sexo = sexo;
+        this.estado = estado;
+    }
+    private int idade;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public Sexo getSexo() {
+        return sexo;
+    }
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+    public UnidadeFederativa getEstado() {
+        return estado;
+    }
+    public void setEstado(UnidadeFederativa estado) {
+        this.estado = estado;
+    }
+    private int id;
+    private double salario;
+    private Sexo sexo;
+    private UnidadeFederativa estado;
+    @Override
+    public String toString() {
+        return "Pessoa [nome=" + nome + ", idade=" + idade + ", id=" + id + ", salario=" + salario + ", sexo=" + sexo
+                + ", estado=" + estado + "]";
+    }
+
     
 
 }
